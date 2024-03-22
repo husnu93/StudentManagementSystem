75d47cfe5c210f7043f3eb435f153afe50dc896f
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-03-23 01:25:12 +0300

    Update TeacherController.java

--- src/main/java/com/project/controller/user/TeacherController.java
+++ src/main/java/com/project/controller/user/TeacherController.java
@@ -28,6 +28,7 @@ public class TeacherController {
       return ResponseEntity.ok(teacherService.saveTeacher(teacherRequest));
   }
 
+
   @PutMapping("/update/{userId}") //http://localhost:8080/teacher/update/1 + PUT + JSON
   @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
     public ResponseMessage<TeacherResponse> uptadeTeacherForManagers(@RequestBody @Valid TeacherRequest teacherRequest,
