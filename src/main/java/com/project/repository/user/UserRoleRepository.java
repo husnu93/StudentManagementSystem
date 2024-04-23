b16d73142ba88aa6d55c8a06f5f1484a79150139
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-04-24 00:01:36 +0300

    Update UserRoleRepository.java

--- src/main/java/com/project/repository/user/UserRoleRepository.java
+++ src/main/java/com/project/repository/user/UserRoleRepository.java
@@ -3,7 +3,7 @@ package com.project.repository.user;
 import com.project.entity.concretes.user.UserRole;
 import com.project.entity.enums.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query; 
 
 import java.util.Optional;
 
