21a43ba32d99671fb56938c129ba2e147ef211c0
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-06-24 16:42:23 +0300

    Update ResourceNotFoundException.java

--- src/main/java/com/project/exception/ResourceNotFoundException.java
+++ src/main/java/com/project/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.project.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND) // 404
+@ResponseStatus(HttpStatus.NOT_FOUND) // 404 not found
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message);
